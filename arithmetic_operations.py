f286afc779c82309e8ba009217ab45dbe540d15a
TheosphilosNisingizwe1 <theophilenisingizwe04@gmail.com>
2026-02-01 10:17:44 +0200

    Week 5 arithmetic_operations.py

---
+++ arithmetic_operations.py
@@ -0,0 +1,21 @@
+def perform_operation(num1, num2, operation):
+ if operation == "addition":
+  return num1 + num2
+ elif operation == "subtraction":
+  return num1 - num2
+ elif operation == "multiplication":
+  return num1 * num2
+ elif operation == "division":
+  match num2:
+   case 0:
+     print("Can't divide by zero")
+   case _:
+    return num1 / num2
+ else:
+  print("Invalid input")  
+
+ 
+ 
+
+
+     

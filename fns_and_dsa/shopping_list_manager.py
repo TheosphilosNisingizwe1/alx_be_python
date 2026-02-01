d0aa7ab005236afb3450d6bb3bdfedfa90928d7f
TheosphilosNisingizwe1 <theophilenisingizwe04@gmail.com>
2026-02-01 12:04:56 +0200

    Fix prompt message for adding an item

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -15,7 +15,7 @@ def main():
 
         if choice == '1':
             # Add an item
-            item = input("Enter the item name to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f"'{item}' has been added to the shopping list.")
 

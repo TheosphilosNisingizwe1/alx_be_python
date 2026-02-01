29acf30e17ddb65d0e7386048f9dd12240d1cc8d
TheosphilosNisingizwe1 <theophilenisingizwe04@gmail.com>
2026-02-01 11:43:03 +0200

    Fix division by zero handling in arithmetic_operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,10 +6,9 @@ def perform_operation(num1, num2, operation):
  elif operation == "multiply":
   return num1 * num2
  elif operation == "division":
-  match num2:
-   case 0:
-     print("Can't divide by zero")
-   case _:
-    return num1 / num2
+  if num2 = 0:
+   print("Can't divide by zero")
+  else:
+   return num1 / num2
  else:
   print("Invalid input")  
